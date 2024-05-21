747c1d99e6cde06dd976180799e51dfb55163fd2
Matheus <matheus271112@gmail.com>
2024-05-21 16:50:37 -0300

    final 1

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 $(document).ready(function () {
+    var flag = false;
     $('#start').click(function () {
-        $('.box').slideDown();
+        $('.add').slideDown();
         $('header').slideUp();
     });
 
-    $('#form1').on('submit', function(e){
+    $('#form1').on('submit', function (e) {
         e.preventDefault();
         const nameValue = $('#name').val();
+        $('.box').slideDown();
         $('#form2').before(`<h2 id='title'>${nameValue}</h2>`);
-        $('form').slideUp();
+        $(".add").css("display", "none");
         $('#form2').slideDown();
     });
 
-        $('#form2').on('submit', function(e) {
-            e.preventDefault();
-            const nameValue2 = $('#task').val();
-            $('ul').append(`<li>${nameValue2}</li>`);
-            $('#task').val('') ;
-        });
+    $('#form2').on('submit', function (e) {
+        e.preventDefault();
+        const nameValue2 = $('#task').val();
+        $('ul').append(`<li>${nameValue2}</li>`);
+        $('#task').val('');
     });
-    
+
+    const textD = $(this).css("text-decoration", "line-through");
+
+    $('ul').on('click', 'li', function () {
+
+        if (flag) {
+            $(this).css("text-decoration", "");
+            flag = false;
+        } else {
+            flag = true;
+            $(this).css("text-decoration", "line-through");
+        }
+    });
+});
+
